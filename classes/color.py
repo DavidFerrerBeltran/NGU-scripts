c343438d6e45959b9cc3a9b42e573f60e95815f2
DavidFerrerBeltran <ferrerdavid96@gmail.com>
2019-11-22 12:58:40 +0100

    Update color.py
    Defined types RGB_color and hex_color

--- classes/color.py
+++ classes/color.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 from typing import Tuple
 
+RGB_color = Tuple[int, int, int]
+hex_color = str
+
 class Color:
     # Do not use Color(rgb, hex) to initialize a Color object
     # Use RGB(r) or hex(h) instead
-    def __init__(self :Color, rgb :Tuple[int, int, int], hex :str) -> None:
+    def __init__(self :Color, rgb :RGB_color, hex :hex_color) -> None:
         self.rgb = rgb
         self.hex = hex
         
@@ -16,25 +19,25 @@ class Color:
         return self.hex
     
     @staticmethod
-    def RGB(r :Tuple[int, int, int]) -> Color:
+    def RGB(r :RGB_color) -> Color:
         return Color(r, Color.rgb_to_hex(r))
         
     @staticmethod
-    def hex(h :str) -> Color:
+    def hex(h :hex_color) -> Color:
         return Color(Color.hex_to_rgb(h), h)
         
-    def get_RGB(self :Color) -> Tuple[int, int, int]:
+    def get_RGB(self :Color) -> RGB_color:
         return self.rgb
     
-    def get_hex(self :Color) -> str:
+    def get_hex(self :Color) -> hex_color:
         return self.hex
     
     @staticmethod
-    def rgb_to_hex(tup :Tuple[int, int, int]) -> str:
+    def rgb_to_hex(tup :RGB_color) -> hex_color:
         """Convert RGB value to hex."""
         return '%02x%02x%02x'.upper() % (tup[0], tup[1], tup[2])
 
     @staticmethod
-    def hex_to_rgb(str :str) -> Tuple[int, int, int]:
+    def hex_to_rgb(str :hex_color) -> RGB_color:
         """Convert hex value to RGB."""
         return tuple(int(str[i:i + 2], 16) for i in (0, 2, 4))
